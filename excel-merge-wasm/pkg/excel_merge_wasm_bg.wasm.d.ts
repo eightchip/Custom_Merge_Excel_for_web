1ccb5fc16ea447114ca42846501409d125a3dbef
宇太郎 清水 <eightchip@yahoo.co.jp>
2026-01-11 20:39:09 +0900

    Add pre-built WASM module files for Vercel deployment

---
+++ excel-merge-wasm/pkg/excel_merge_wasm_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: () => void;
+export const compare_files: (a: number, b: number) => [number, number];
+export const split_file: (a: number, b: number) => [number, number];
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
